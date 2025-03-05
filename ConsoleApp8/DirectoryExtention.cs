30312d61d8bea9fd306ab5d6422a2d569d5552e3
vladkop2025 <vladkop@mail.ru>
2025-03-05 08:56:14 +0300

    DirectoryExtention

---
+++ ConsoleApp8/DirectoryExtention.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    public static class DirectoryExtention
+    {
+        public static long DirSize(DirectoryInfo d)
+        {
+            long size = 0;
+            FileInfo[] fis = d.GetFiles();
+            //подсчет размера всех файлов в выбранной директории
+            foreach (FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+
+            DirectoryInfo[] dis = d.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            //подсчет размера всех директорий, вложенных в выбранную директорию
+            {
+                size += DirSize(d);
+            }
+            return size;
+        }
+    }
+}
